2b796e5a4848a295192adf1313770eaede3a8702
arvin <83908258+arvinsalehi@users.noreply.github.com>
2025-01-18 03:13:21 -0500

    Update sketch_esp32_hello_world_mors.ino

--- sketch_esp32_hello_world_mors.ino
+++ sketch_esp32_hello_world_mors.ino
@@ -8,68 +8,68 @@ static const BaseType_t app_cpu = 1;
 #endif
 
 const char* MORSCODE[][2] = {
-    {"A", ".-"},    // A
-    {"B", "-..."},  // B
-    {"C", "-.-."},  // C
-    {"D", "-.."},   // D
-    {"E", "."},     // E
-    {"F", "..-."},  // F
-    {"G", "--."},   // G
-    {"H", "...."},  // H
-    {"I", ".."},    // I
-    {"J", ".---"},  // J
-    {"K", "-.-"},   // K
-    {"L", ".-.."},  // L
-    {"M", "--"},    // M
-    {"N", "-."},    // N
-    {"O", "---"},   // O
-    {"P", ".--."},  // P
-    {"Q", "--.-"},  // Q
-    {"R", ".-."},   // R
-    {"S", "..."},   // S
-    {"T", "-"},     // T
-    {"U", "..-"},   // U
-    {"V", "...-"},  // V
-    {"W", ".--"},   // W
-    {"X", "-..-"},  // X
-    {"Y", "-.--"},  // Y
-    {"Z", "--.."},  // Z
-    {"1", ".----"}, // 1
-    {"2", "..---"}, // 2
-    {"3", "...--"}, // 3
-    {"4", "....-"}, // 4
-    {"5", "....."}, // 5
-    {"6", "-...."}, // 6
-    {"7", "--..."}, // 7
-    {"8", "---.."}, // 8
-    {"9", "----."}, // 9
-    {"0", "-----"}, // 0
+    {"A", ".-"},    
+    {"B", "-..."}, 
+    {"C", "-.-."},  
+    {"D", "-.."},   
+    {"E", "."},     
+    {"F", "..-."},  
+    {"G", "--."},   
+    {"H", "...."},  
+    {"I", ".."},    
+    {"J", ".---"},  
+    {"K", "-.-"},   
+    {"L", ".-.."},  
+    {"M", "--"},    
+    {"N", "-."},    
+    {"O", "---"},   
+    {"P", ".--."},  
+    {"Q", "--.-"},  
+    {"R", ".-."},   
+    {"S", "..."},   
+    {"T", "-"},     
+    {"U", "..-"},   
+    {"V", "...-"},  
+    {"W", ".--"},   
+    {"X", "-..-"},  
+    {"Y", "-.--"},  
+    {"Z", "--.."},  
+    {"1", ".----"}, 
+    {"2", "..---"}, 
+    {"3", "...--"}, 
+    {"4", "....-"}, 
+    {"5", "....."}, 
+    {"6", "-...."}, 
+    {"7", "--..."}, 
+    {"8", "---.."}, 
+    {"9", "----."}, 
+    {"0", "-----"}, 
 };
 
 const int T = 100;  
 
 const int morseTimingDictionary[][2] = {
-    {1, T},     // Dot (.) = 1T
-    {2, 3 * T}, // Dash (-) = 3T
-    {3, T},     // Space between elements of the same letter = 1T
-    {4, 3 * T}, // Space between letters = 3T
-    {5, 7 * T}, // Space between words = 7T
+    {1, T},     
+    {2, 3 * T}, 
+    {3, T},     
+    {4, 3 * T}, 
+    {5, 7 * T}, 
 };
 
-// pins
 static const int led_pin = LED_BUILTIN;
 
 const char* searchMorsCode(char letter) {
   for(int i; i < 36; i++) {
-    if (MORSCODE[i][0][0] = letter) { // MORSCODE[i][0][0] to point to char A. wherease MORSCODE[i][0] points to string A
+    if (MORSCODE[i][0][0] = letter) { 
       return MORSECODE[i][1];
     }
     return nullptr
   }
 }
-// Blink a LED;
+
+
 void toggleLED(void *parameter) {
-  //TODO complete this func. NOT WORKING YET
+  //TODO
   while(1) {
     digitalWrite(led_pin, HIGH);
     vTaskDelay(500 / portTICK_PERIOD_MS);
