a15d452a14347f2419bf509b5c1f6970d9055a81
arvin <83908258+arvinsalehi@users.noreply.github.com>
2025-04-01 18:53:09 -0400

    Update sketch_esp32_hello_world_mors.ino
    More modularity

--- sketch_esp32_hello_world_mors.ino
+++ sketch_esp32_hello_world_mors.ino
@@ -7,53 +7,51 @@ static const BaseType_t app_cpu = 0;
 static const BaseType_t app_cpu = 1;
 #endif
 
-const char* MORSCODE[][2] = {
-    {"A", ".-"},    
-    {"B", "-..."}, 
-    {"C", "-.-."},  
-    {"D", "-.."},   
-    {"E", "."},     
-    {"F", "..-."},  
-    {"G", "--."},   
-    {"H", "...."},  
-    {"I", ".."},    
-    {"J", ".---"},  
-    {"K", "-.-"},   
-    {"L", ".-.."},  
-    {"M", "--"},    
-    {"N", "-."},    
-    {"O", "---"},   
-    {"P", ".--."},  
-    {"Q", "--.-"},  
-    {"R", ".-."},   
-    {"S", "..."},   
-    {"T", "-"},     
-    {"U", "..-"},   
-    {"V", "...-"},  
-    {"W", ".--"},   
-    {"X", "-..-"},  
-    {"Y", "-.--"},  
-    {"Z", "--.."},  
-    {"1", ".----"}, 
-    {"2", "..---"}, 
-    {"3", "...--"}, 
-    {"4", "....-"}, 
-    {"5", "....."}, 
-    {"6", "-...."}, 
-    {"7", "--..."}, 
-    {"8", "---.."}, 
-    {"9", "----."}, 
-    {"0", "-----"}, 
-};
+void dit() {
+    printf("dit ");
+}
 
-const int T = 100;  
+void dat() {
+    printf("dat ");
+}
 
-const int morseTimingDictionary[][2] = {
-    {1, T},     
-    {2, 3 * T}, 
-    {3, T},     
-    {4, 3 * T}, 
-    {5, 7 * T}, 
+const void* MORSECODE[][3] = {
+    {"A", dit, dat},    
+    {"B", dat, dit, dit, dit}, 
+    {"C", dat, dit, dat, dit},  
+    {"D", dat, dit, dit},   
+    {"E", dit},     
+    {"F", dit, dit, dat, dit},  
+    {"G", dat, dat, dit},   
+    {"H", dit, dit, dit, dit},  
+    {"I", dit, dit},    
+    {"J", dit, dat, dat, dat},  
+    {"K", dat, dit, dat},   
+    {"L", dit, dat, dit, dit},  
+    {"M", dat, dat},    
+    {"N", dat, dit},    
+    {"O", dat, dat, dat},   
+    {"P", dit, dat, dat, dit},  
+    {"Q", dat, dat, dit, dat},  
+    {"R", dit, dat, dit},   
+    {"S", dit, dit, dit},   
+    {"T", dat},     
+    {"U", dit, dit, dat},   
+    {"V", dit, dit, dit, dat},  
+    {"W", dit, dat, dat},   
+    {"X", dat, dit, dit, dat},  
+    {"Y", dat, dit, dat, dat},  
+    {"Z", dat, dat, dit, dit},  
+    {"1", dit, dat, dat, dat, dat}, 
+    {"2", dit, dit, dat, dat, dat}, 
+    {"3", dit, dit, dit, dat, dat}, 
+    {"4", dit, dit, dit, dit, dat}, 
+    {"5", dit, dit, dit, dit, dit}, 
+    {"6", dat, dit, dit, dit, dit}, 
+    {"7", dat, dat, dit, dit, dit}, 
+    {"8", dat, dat, dat, dit, dit}, 
+    {"9", dat, dat, dat, dat, dit}, 
+    {"0", dat, dat, dat, dat, dat}, 
 };
 
 static const int led_pin = LED_BUILTIN;
